2396802934dd9a7372e0f99802f54d198ed3326c
Rodrigo Chibana <rchibana@rchibana-VirtualBox.(none)>
2017-01-29 23:43:26 -0200

    Created the controllers and entities

---
+++ src/main/java/br/com/todo/controller/task/TaskController.java
@@ -0,0 +1,17 @@
+package br.com.todo.controller.task;
+
+import br.com.todo.entity.task.TaskEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by rchibana on 1/29/17.
+ */
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+
+}
+
+
---
+++ src/main/java/br/com/todo/entity/task/TaskEntity.java
@@ -0,0 +1,42 @@
+package br.com.todo.entity.task;
+
+/**
+ * Created by rchibana on 1/29/17.
+ */
+public class TaskEntity {
+
+    public Long id;
+    public String name;
+    public String description;
+
+    public TaskEntity(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public TaskEntity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
