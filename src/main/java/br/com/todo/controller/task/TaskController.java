618fd476dbfd60c4a988465d6f2c005b8903c3b2
Rodrigo Chibana <rchibana@rchibana-VirtualBox.(none)>
2017-02-01 01:01:53 -0200

    Added the thymeleaf and changed the entity name

--- src/main/java/br/com/todo/controller/task/TaskController.java
+++ src/main/java/br/com/todo/controller/task/TaskController.java
@@ -1,16 +1,25 @@
 package br.com.todo.controller.task;
 
-import br.com.todo.entity.task.TaskEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by rchibana on 1/29/17.
  */
 
-@RestController
+@Controller
 @RequestMapping("/task")
 public class TaskController {
 
+    @GetMapping()
+    public String get(final Model model){
+
+        model.addAttribute("name", "rodrigo");
+
+        return "task/task";
+
+    }
 
 }
 
--- src/main/java/br/com/todo/entity/task/Task.java
+++ src/main/java/br/com/todo/entity/task/Task.java
@@ -1,21 +1,24 @@
 package br.com.todo.entity.task;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * Created by rchibana on 1/29/17.
  */
-public class TaskEntity {
+public class Task {
 
+    @Id
     public Long id;
     public String name;
     public String description;
 
-    public TaskEntity(Long id, String name, String description) {
+    public Task(Long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public TaskEntity(String name, String description) {
+    public Task(String name, String description) {
         this.name = name;
         this.description = description;
     }
